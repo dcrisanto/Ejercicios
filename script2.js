59032ae0217fd7ad992dbf715ed70bcc4d9e2de7
dcrisanto <33395560+dcrisanto@users.noreply.github.com>
2019-06-12 19:07:01 -0500

    Create script2.js

---
+++ script2.js
@@ -0,0 +1,6 @@
+//Ejercicio 2: Concatenar los valores del array en un String
+//separados por guión
+
+array2 = ["hola","Hola","Dorelly","mundo","Juan Diego","Roxana"];
+
+$('#array2').html(array2.join('-'))
