3152e0c594b1563aab60059ce492c1d03347c5e4
dcrisanto <33395560+dcrisanto@users.noreply.github.com>
2019-06-12 19:14:43 -0500

    Create script4.js

---
+++ script4.js
@@ -0,0 +1,20 @@
+//Ejercicio 4: Obtener un array con todos los elementos
+// que son únicos
+array4 = ['hola', 'Hola', 'Dorelly', 'mundo', 'Juan Diego', 'mundo', 'Roxana'];
+arrayUnique = [];
+elementsRepeated = [];
+newArray4 = [];
+for(var z=0; z < array4.length; z++){
+  if(!arrayUnique.includes(array4[z])){
+    arrayUnique.push(array4[z]);
+  }
+  else{
+    elementsRepeated.push(array4[z]);
+  }
+}
+for(var w = 0; w < elementsRepeated.length; w++){
+  arrayUnique.splice((arrayUnique.indexOf(elementsRepeated[w])),1);
+}
+
+$('#array4').html(JSON.stringify(array4))
+$('#newArray4').html(JSON.stringify(arrayUnique))
